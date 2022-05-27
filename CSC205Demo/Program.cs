eb65af064630aef5e8ec4009d3a72039caad2e88
Guangyan Li <GLi@stmartin.edu>
2022-05-27 11:49:52 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-public class Program
+namespace CSC205Week02
 {
-    static void Main()
+    class Program
     {
-        int i = 1;
-        while (i <= 6)
+        static void Main(string[] args)
         {
-            WriteMultiples(i);
-            i++;
+            int hour = 0, minute = 14, second = 0;
+            int secondSinceMidnight;
+            secondSinceMidnight = hour * 60 * 60 + minute * 60 + second;
+            Console.WriteLine("Number of seconds since midnight: " + secondSinceMidnight);
+            Console.WriteLine("Number of seconds remaining in the day: " + (24 * 60 * 60 - secondSinceMidnight));
+            Console.WriteLine("Fraction of the day that has passed: " + (double)secondSinceMidnight / (24 * 60 * 60) * 100);
         }
-    }
-    static void WriteMultiples(int n)
-    {
-        int i = 1;
-        while (i <= 6)
-        {
-            Console.Write(n * i + " ");
-            i++;
-        }
-        Console.WriteLine();
     }
 }
