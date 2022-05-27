d0ab0ff51740b480b7fad749d00c3b5fc5eba1ba
Guangyan Li <GLi@stmartin.edu>
2022-05-27 11:15:30 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -3,11 +3,17 @@ public class Program
 {
     static void Main()
     {
+
+        Sum(10);
+    }
+
+    static void Sum(int n)
+    {
         int result = 0, i = 1;
-        while (i <= 10)
+        while (i <= n)
         {
-            result += i;
-            i++;
+            result += i; // result = result + i;
+            i++; // i = i + 1;
         }
         Console.WriteLine(result);
     }
