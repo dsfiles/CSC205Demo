84d610fac45d7f7a4ed09148b0539e6dd497a3bd
Guangyan Li <GLi@stmartin.edu>
2022-05-27 11:10:13 -0700

    aa

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,40 +1,14 @@
 ﻿using System;
-
-namespace CSC205Demo
+public class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int result = 0, i = 1;
+        while (i <= 10)
         {
-            Console.WriteLine(Fibonacci(10));
-            Console.WriteLine(Fibonacci2(10));
+            result += i;
+            i++;
         }
-
-        public static double Fibonacci(int n)
-        {
-            if (n == 0 || n == 1)
-            {
-                return 1;
-            }
-            else
-            {
-                return Fibonacci(n - 1) + Fibonacci(n - 2);
-            }
-        }
-
-        public static double Fibonacci2(int n)
-        {
-            double n1 = 1, n2 = 1, temp;
-            int i = 0;
-            while (i < n-1)
-            {
-                temp = n1 + n2;
-                n1 = n2;
-                n2 = temp;
-                i = i + 1;
-            }
-            return n2;
-        }
-
+        Console.WriteLine(result);
     }
 }
