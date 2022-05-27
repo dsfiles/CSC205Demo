f78138eabfac8967553fecbf0e7729fed4092f73
Guangyan Li <GLi@stmartin.edu>
2022-05-27 10:43:55 -0700

    Add project files.

---
+++ CSC205Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC205Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
