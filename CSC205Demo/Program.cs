46ba72e39e3b1241d3a67d77de129bd466c500c5
Guangyan Li <GLi@stmartin.edu>
2022-06-01 10:44:08 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,38 +1,23 @@
-﻿using System;
-namespace CSC205Quiz02
+﻿public class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int i = 1;
+        while (i <= 6)
         {
-            Console.WriteLine(Median(1, 2, 3));
-            Console.WriteLine(Median(1, 3, 2));
-            Console.WriteLine(Median(2, 1, 3));
-            Console.WriteLine(Median(2, 3, 1));
-            Console.WriteLine(Median(3, 2, 1));
-            Console.WriteLine(Median(3, 1, 2));
-            Console.WriteLine(Median(2, 2, 1));
-            Console.WriteLine(Median(2, 2, 3));
-            Console.WriteLine(Median(2, 3, 2));
-            Console.WriteLine(Median(1, 2, 2));
-            Console.WriteLine(Median(3, 2, 2));
+            WriteMultiples(i);
+            i++;
         }
-
-        static double Median(double x, double y, double z)
-        {
-            // Checking for y
-            if ((x < y && y < z) || (z < y && y < x))
-            {
-                return y;
-            }
-            // Checking for z
-            else if ((x < z && z < y) || (y < z && z < x))
-            {
-                return z;
-            }
-            // Default to x since we know at this point x is middle
-            else return x;
-        }
-
     }
+    static void WriteMultiples(int n)
+    {
+        int j = 1;
+        while (j <= 6)
+        {
+            System.Console.Write(n * j + "\t");
+            j++;
+        }
+        System.Console.WriteLine();
+    }
+
 }
