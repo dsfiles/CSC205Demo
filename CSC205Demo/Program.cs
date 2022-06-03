f0de5d9de199866eba53f2d63325c536dc675e50
Guangyan Li <GLi@stmartin.edu>
2022-06-03 10:59:43 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        int num;
-        string myStr = "25";
-        num = int.Parse(myStr);
-        Console.WriteLine("int.Parse method result: " + num);
-        num = Convert.ToInt32(myStr);
-        Console.WriteLine("Convert.ToInt32 method result: " + num);
+        string str;
+        Console.WriteLine("Please enter your name:");
+        str = Console.ReadLine();
+        Console.WriteLine("Please enter your age (1-99):");
+        int age = 0;
+        age = Convert.ToInt32(Console.ReadLine());
+        Greeting(str, age);
+    }
+    public static void Greeting(string name, int age)
+    {
+        Console.WriteLine("Hello " + name + "!");
+        if (age > 21)
+            Console.WriteLine("you can buy alcohol!");
+        else
+            Console.WriteLine("Sorry, you can't buy alcohol!");
     }
 }
