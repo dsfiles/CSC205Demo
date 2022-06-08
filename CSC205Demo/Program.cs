8ac7c81f017f56123c994c7a913f94095c6fd00c
Guangyan Li <GLi@stmartin.edu>
2022-06-08 09:35:48 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.IO;
-
-namespace ThinkSharp
+﻿// Write down the output then run the program to check your answer
+using System;
+class CSC205Week04ExerciseOne
 {
-    public class ListFile
+    static void Main(string[] args)
     {
-        public static void Main()
-        {
-            try
-            {
-                processFile("words.txt");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("That didn't work. Here's why:");
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
-        } }
-
+        Method(3, 5);
+        Method(3, 5.0);
+        Method(3.0, 5);
+        Method(3.0, 5.0);
     }
-}
+    public static void Method(int a, int b)
+    {
+        Console.WriteLine($"a: {a.GetType()}, b: {b.GetType()}");
+    }
+    public static void Method(double a, int b)
+    {
+        Console.WriteLine($"a: {a.GetType()}, b: {b.GetType()}");
+    }
+    public static void Method(double a, double b)
+    {
+        Console.WriteLine($"a: {a.GetType()}, b: {b.GetType()}");
+    }
 }
