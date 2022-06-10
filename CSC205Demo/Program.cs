1183e554ecbc7e3dbff5d7a3078992c06c35672b
Guangyan Li <GLi@stmartin.edu>
2022-06-10 10:30:33 -0700

    c

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -6,15 +6,16 @@ namespace CSC205Demo
         public static void Main(string[] args)
         {
             var t = new Time();
-            Console.WriteLine($"{t.hour}:{t.minute}:{t.second}");
+            //Console.WriteLine($"{t.hour}:{t.minute}:{t.second}");
             
             var t2 = new Time(11, 5, 23.4567);
-            Console.WriteLine($"{t2.hour}:{t2.minute}:{t2.second}");
+            //Console.WriteLine($"{t2.hour}:{t2.minute}:{t2.second}");
 
             Time.WriteTime(t2);
             Time.WriteTime2(t2);
-
             t2.WriteTime3();
+            t2.WriteTime4(t2);
+
         }
     }
     
--- CSC205Demo/Time.cs
+++ CSC205Demo/Time.cs
@@ -11,7 +11,7 @@ namespace CSC205Demo
 
         public Time()
         {
-            this.hour = 23;
+            this.hour = 0;
             this.minute = 0;
             this.second = 0.0;
         }
@@ -22,24 +22,27 @@ namespace CSC205Demo
             this.second = s;
         }
 
-        public static void WriteTime(Time t)
-        {
-            Console.Write(t.hour);
-            Console.Write(":");
-            Console.Write(t.minute);
-            Console.Write(":");
-            Console.WriteLine(t.second);
-        }
-
-        public static void WriteTime2(Time t)
-        {   // a version that uses String.Format
+        public static void WriteTime(Time t) {
             Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
             t.hour, t.minute, (int)t.second));
         }
 
-        public void WriteTime3()
-        {  // a version uses this
-            Console.WriteLine(this.hour + ":" + this.minute + ":" + this.second);
+        public static void WriteTime2(Time t) {
+            Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
+            t.hour, t.minute, (int)t.second));
         }
+        public void WriteTime3() {
+            Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
+             this.hour, this.minute, (int)this.second));
+        }
+        public void WriteTime4(Time t) {
+            Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
+             t.hour, t.minute, (int)t.second));
+        }
+
+        //Time.WriteTime(t2);
+        //Time.WriteTime2(t2);
+        //t2.WriteTime3();
+        //t2.WriteTime4(t2);
     }
 }
