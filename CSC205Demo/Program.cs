3bb6f2405a07bf6d78d4de52c6dc381109b8ce42
Guangyan Li <GLi@stmartin.edu>
2022-06-10 11:09:23 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -8,28 +8,16 @@ namespace CSC205Demo
         public string isbn;
         public double price;
         public Book()
-        {
-            title = "";
-            isbn = "";
-            price = 0.0;
+        {   title = ""; isbn = ""; price = 0.0; }
+        public Book(string t, string i, double p) {
+            title = t; isbn = i; price = p;
         }
-
-        public Book(string t, string i, double p)
-        {
-            title = t;
-            isbn = i;
-            price = p;
-        }
-
-        public void Display()
-        { 
+        public void Display(){ 
             Console.WriteLine($"Title: {this.title}, ISBN: {this.isbn}, Price: {this.price}"); 
         }
-
     }
     public class Program {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             var book1 = new Book();
             book1.Display();
 
