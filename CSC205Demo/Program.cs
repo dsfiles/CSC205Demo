f71aec70da1832bdc5a49690c31d042936ca85f8
Guangyan Li <GLi@stmartin.edu>
2022-06-10 10:53:48 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -7,10 +7,10 @@ namespace CSC205Demo
         public string title;
         public string isbn;
         public double price;
-        public Book()
-        {
-            title = ""; isbn = "";price = 0.0;
-        }
+        //public Book()
+        //{
+        //    title = ""; isbn = "";price = 0.0;
+        //}
     }
 
     public class Program
@@ -19,10 +19,10 @@ namespace CSC205Demo
         {
             Book book1 = new Book();
             Console.WriteLine(book1.title);
-            //book1.title = "C# 9 and .NET 5";
-            //book1.isbn = "180056810X";
-            //book1.price = 42.74;
-            //Console.WriteLine(book1.title);
+            book1.title = "C# 9 and .NET 5";
+            book1.isbn = "180056810X";
+            book1.price = 42.74;
+            Console.WriteLine(book1.title);
 
             //Book book2 = new Book();
             //book2.title = "C# in Depth";
