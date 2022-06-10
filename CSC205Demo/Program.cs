7896547a1044e0c2ff694585cfafcba486e8e6e2
Guangyan Li <GLi@stmartin.edu>
2022-06-10 10:39:46 -0700

    d

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -5,16 +5,20 @@ namespace CSC205Demo
     {
         public static void Main(string[] args)
         {
-            var t = new Time();
+            var t1 = new Time();
             //Console.WriteLine($"{t.hour}:{t.minute}:{t.second}");
             
             var t2 = new Time(11, 5, 23.4567);
             //Console.WriteLine($"{t2.hour}:{t2.minute}:{t2.second}");
 
-            Time.WriteTime(t2);
-            Time.WriteTime2(t2);
-            t2.WriteTime3();
+            //Time.WriteTime(t2);
+            //Time.WriteTime2(t2);
+            //t2.WriteTime3();
+            t1.WriteTime4(t1);
+            t1.WriteTime4(t2);
+            t2.WriteTime4(t1);
             t2.WriteTime4(t2);
+
 
         }
     }
