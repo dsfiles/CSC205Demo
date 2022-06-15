a52cea9feb5cf36f2ebe03d064e88787fd63ad9d
Guangyan Li <GLi@stmartin.edu>
2022-06-15 09:59:59 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -4,18 +4,20 @@ namespace CSC205Demo
     using System;
     public class Program {
         public static void Main() {
-            var t1 = new Time();
-            var t2 = new Time(9, 35, 28);
-            // invoke WriteTime1 to display t2
-            Time.WriteTime1(t2);
-            // invoke WriteTime2 to display t2
-            t2.WriteTime2();
-            // two ways to invoke WriteTime3 to display t2
-            t2.WriteTime3(t2);
-            t1.WriteTime3(t2);
-            //public static void WriteTime1(Time t)
-            //public void WriteTime2()
-            //public void WriteTime3(Time t)
+            Time t1 = new Time(11, 58, 31);
+            Time t2 = new Time(11, 58, 31);
+            Console.WriteLine(  Time.IsAfter(t1, t2) );
+
+            //public static bool IsAfter(Time time1, Time time2)
+            //{
+            //    if (time1.hour > time2.hour) return true;
+            //    if (time1.hour < time2.hour) return false;
+            //    if (time1.minute > time2.minute) return true;
+            //    if (time1.minute < time2.minute) return false;
+            //    if (time1.second > time2.second) return true;
+            //    return false;
+            //}
+
         }
     }
 }
--- CSC205Demo/Time.cs
+++ CSC205Demo/Time.cs
@@ -34,5 +34,15 @@ namespace CSC205Demo
             Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}",
              t.hour, t.minute, (int)t.second));
         }
+
+        public static bool IsAfter(Time time1, Time time2)
+        {
+            if (time1.hour > time2.hour) return true;
+            if (time1.hour < time2.hour) return false;
+            if (time1.minute > time2.minute) return true;
+            if (time1.minute < time2.minute) return false;
+            if (time1.second > time2.second) return true;
+            return false;
+        }
     }
 }
