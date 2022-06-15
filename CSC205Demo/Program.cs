3342ae24b2821441300bd3c9d8b83006e57a984e
Guangyan Li <GLi@stmartin.edu>
2022-06-15 10:44:51 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -8,10 +8,9 @@ namespace CSC205Demo
             
             Time t4 = new Time(11, 28, 9);
             //Console.WriteLine(  Time.IsAfter(t1, t2) );
-
-            Time.AddTime2(t3, t4).WriteTime2();
-
+            t4.WriteTime2();
             Time.Increment(t4, 29);
+            t4.WriteTime2();
 
             //public static void Increment(Time time, double secs)
         }
