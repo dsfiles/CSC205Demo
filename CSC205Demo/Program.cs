0496fed87a1c80e1815f3044cf50be17fd4fe526
Guangyan Li <GLi@stmartin.edu>
2022-06-15 11:44:27 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -10,24 +10,22 @@ namespace CSC205Demo
             count[1] = count[0] * 2;
             count[2]++;
             count[3] -= 60;
-
             int[] arr = new int[4];
-            arr = count;
-
+            //arr = count;
+            for (int i = 0; i < count.Length; i++)
+            {
+                arr[i] = count[i];
+            }
             foreach (var item in count) {
                 Console.WriteLine(item);
             } 
-
             Console.WriteLine();
-            
             arr[0] = 1;
-
             foreach (var item in arr){
                 Console.WriteLine(item);
             }
             Console.WriteLine();
-            foreach (var item in count)
-            {
+            foreach (var item in count) {
                 Console.WriteLine(item);
             }
 
