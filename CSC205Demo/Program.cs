e6d08c418eb73c33e542bc6ec19e8b5c4174f75f
GL <GLi@stmartin.edu>
2022-06-15 00:11:40 -0700

    update

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -6,10 +6,11 @@ namespace CSC205Demo
         public static void Main() {
             var t1 = new Time();
             var t2 = new Time(9, 35, 28);
-            Time.WriteTime1(t2);
-            t2.WriteTime2();
-            t1.WriteTime3(t2);
-            t2.WriteTime3(t2);
+            // invoke WriteTime1 to display t2
+            // invoke WriteTime2 to display t2
+            // two ways to invoke WriteTime3 to display t3
+            // 1)
+            // 2)
         }
     }
 }
