bde217d27e9347a6e2b19de606d9dcf493c9333c
Guangyan Li <GLi@stmartin.edu>
2022-06-17 10:10:20 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
-namespace CSC205Demo
+namespace ThinkSharp
 {
-    public class Program {
-        public static void Main()
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteArray(RandomArray(10));
+        }
+        public static int[] RandomArray(int size)
         {
             Random random = new Random();
-            for (int i = 0; i < 10; i++)
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
             {
-                double x = random.NextDouble();
-                Console.WriteLine(x);
+                array[i] = random.Next(0, 100);
             }
-
-
+            return array;
+        }
+        public static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
