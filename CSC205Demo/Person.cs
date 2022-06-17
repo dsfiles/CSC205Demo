6fbb9effb0133e038cc509b66314198861122f86
Guangyan Li <GLi@stmartin.edu>
2022-06-17 11:18:32 -0700

    a

---
+++ CSC205Demo/Person.cs
@@ -0,0 +1,24 @@
+﻿class Person // Parent class
+{
+    public string name;
+    public int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void DisplayNameAge()
+    { System.Console.WriteLine($"{name} is {age} years old."); }
+}
+class Teacher : Person
+{ // Child class 
+    public string subject;
+    public Teacher(string name, int age, string subject) : base(name, age)
+    { this.subject = subject; }
+}
+class Student : Person
+{ // Child class
+    public double gpa;
+    public Student(string name, int age, double gpa) : base(name, age)
+    { this.gpa = gpa; }
+}
--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,50 +1,11 @@
-﻿using System;
-
-public class Vehicle // base class
-{
-    public string model;
-    public int year;
-    public Vehicle(string model, int year)
-    {
-        this.model = model;
-        this.year = year;
-    }
-}
-
-public class Car : Vehicle
-{
-    private int seating_capacity;
-    public Car(string model, int year, int capacity) : base(model, year)
-    {
-        seating_capacity = capacity;
-    }
-    public void DisplayInfo()
-    {
-        Console.WriteLine($"model: {model}, year: {year}, seating capacity: {seating_capacity}");
-    }
-}
-
-public class Truck : Vehicle
-{
-    private int load_capacity;
-    public Truck(string model, int year, int capacity) : base(model, year)
-    {
-        load_capacity = capacity;
-    }
-    public void DisplayInfo()
-    {
-        Console.WriteLine($"model: {model}, year: {model}, load capacity: {load_capacity} tons");
-    }
-}
-
-public class Program
+﻿public class Program
 {
     static void Main(string[] args)
     {
-        var car = new Car("Honda Accord", 2019, 5);
-        var truck = new Truck("kenworth W990", 2020, 50);
-        car.DisplayInfo();
-        truck.DisplayInfo();
+        Person person = new Person("Allan", 21);
+        Teacher teacher = new Teacher("Tom", 55, "Computer Science");
+        teacher.DisplayNameAge();
+        Student student = new Student("Sara", 19, 3.5);
+        student.DisplayNameAge();
     }
 }
-
