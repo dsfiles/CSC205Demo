35f39d844bc0bf045047765a76ae9dd0985c8459
Guangyan Li <GLi@stmartin.edu>
2022-06-24 10:07:30 -0700

    aa

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,49 +1,32 @@
-﻿public class App
+﻿namespace CSC205Quiz06
 {
-    static void Main()
+    public class Rect
     {
-        int[] arr = { 1, 2, 2, 5 };
-        //System.Console.WriteLine(Banana(arr));
-        //System.Console.WriteLine(Apple(arr, 6));
-        System.Console.WriteLine(Grapefruit(arr,1));
-    }
-    // Answer: computes the sum of array a's elements and returns it.
-    public static int Banana(int[] a)
-    {
-        int sum = 0;
-        int i = 0;
-        while (i < a.Length)
+        public double Height { get; set; }
+        public double Length { get; set; }
+        public static double ComputeArea1(Rect rec)
         {
-            sum = sum + a[i];
-            i++;
+            return rec.Height * rec.Length;
         }
-        return sum;
+        public double ComputeArea2()
+        {
+            return Height * Length;
+        }
     }
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create a new Rect object call r using the default constructor
+            // Invoke ComputeArea1 and display the area of r 
+            // set both r's Height and Length to 3;
+            // Invoke ComputeArea2 to display the area of r
+            //Answers:
+            var r = new Rect();
+            System.Console.WriteLine(Rect.ComputeArea1(r));
+            r.Height = r.Length = 3.0;
+            System.Console.WriteLine(r.ComputeArea2());
 
-    // Answer: counts the number of times that p appears in the array a
-    public static int Apple(int[] a, int p)
-    {
-        int i = 0;
-        int count = 0;
-        while (i < a.Length)
-        {
-            if (a[i] == p)
-            {
-                count++;
-            }
-            i++;
         }
-        return count;
-    }
-
-    // Answer: return the index of the first appearance of integer p in array a, or -1 if not found
-    public static int Grapefruit(int[] a, int p)
-    {
-        for (int i = 0; i < a.Length; i++)
-        {
-            if (a[i] == p) 
-                return i;
-        }
-        return -1;
     }
 }
