8672bb1574a4833f96e96ed40dded26c4df23693
Guangyan Li <GLi@stmartin.edu>
2022-06-24 09:56:12 -0700

    c

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,68 +1,21 @@
-﻿//Question 1
-
-using System;
-using System.Collections.Generic;
-class Doubloon
-
+﻿public class App
 {
-    static bool IsDoubloon(string s)
+    static void Main()
     {
-
-        IDictionary<char, int> doubloon = new Dictionary<char, int>();
-        // sets result to true by default
-        bool result = true;
-        // converts string to all lowercase values so characters can be properly evaluated
-        s = s.ToLower();
-        // for loop iterates through each letter of the string
-        foreach (char c in s)
-        {
-            // if the letter is already a key in the dictionary, add one to tally
-            if (doubloon.ContainsKey(c))
-            {
-                doubloon[c]++;
-            }
-
-            // if the letter is not yet a key in the dictionary, add the key/value pair with the value set to one
-            else
-            {
-                doubloon.Add(c, 1);
-            }
-        }
-
-
-
-        //iterates throguh dictionary, checking if all characters in string ('keys') appeared exactly twice
-
-        // if any characters appeared more or less than twice, the result is changed to false
-
-        foreach (char key in doubloon.Keys)
-
-        {
-
-            if (doubloon[key] != 2)
-
-            {
-
-                result = false;
-
-            }
-
-        }
-
-
-
-        return result;
+        int[] arr = { 1, 2, 3 };
+        System.Console.WriteLine( Banana(arr));
 
     }
-
-    static void Main(string[] args)
-
+    // Answer: computes the sum of array a's elements and returns it.
+    public static int Banana(int[] a)
     {
-
-        Console.WriteLine(IsDoubloon("Appeases"));
-
-        Console.WriteLine(IsDoubloon("Appeasesa"));
-
+        int grape = 0;
+        int i = 0;
+        while (i < a.Length)
+        {
+            grape = grape + a[i];
+            i++;
+        }
+        return grape;
     }
-
 }
