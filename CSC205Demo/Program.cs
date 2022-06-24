0bd71f8e9c67a9d486b65b988f48c3aa6bbf5015
Guangyan Li <GLi@stmartin.edu>
2022-06-24 10:05:22 -0700

    a

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -2,20 +2,48 @@
 {
     static void Main()
     {
-        int[] arr = { 1, 2, 3 };
-        System.Console.WriteLine( Banana(arr));
-
+        int[] arr = { 1, 2, 2, 5 };
+        //System.Console.WriteLine(Banana(arr));
+        //System.Console.WriteLine(Apple(arr, 6));
+        System.Console.WriteLine(Grapefruit(arr,1));
     }
     // Answer: computes the sum of array a's elements and returns it.
     public static int Banana(int[] a)
     {
-        int grape = 0;
+        int sum = 0;
         int i = 0;
         while (i < a.Length)
         {
-            grape = grape + a[i];
+            sum = sum + a[i];
             i++;
         }
-        return grape;
+        return sum;
+    }
+
+    // Answer: counts the number of times that p appears in the array a
+    public static int Apple(int[] a, int p)
+    {
+        int i = 0;
+        int count = 0;
+        while (i < a.Length)
+        {
+            if (a[i] == p)
+            {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+
+    // Answer: return the index of the first appearance of integer p in array a, or -1 if not found
+    public static int Grapefruit(int[] a, int p)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] == p) 
+                return i;
+        }
+        return -1;
     }
 }
