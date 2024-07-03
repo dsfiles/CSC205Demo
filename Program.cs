df04d10b1985d5b70dd7f6059f8530227f77bb45
gli <gli@SMU13041>
2024-07-03 15:42:29 -0700

    sadfsda

--- Program.cs
+++ Program.cs
@@ -4,33 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSC205Demo
-{
-    internal class Program
-    {
-
-        public static void Zoop()
-        {
+namespace CSC205Demo{
+    internal class Program{
+        public static void Zoop() {
             Baffle();
             Console.Write("You wugga ");
             Baffle();
         }
-
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args){
             Console.Write("No, I ");
             Zoop();
             Console.Write("I ");
             Baffle();
         }
-
-        public static void Baffle()
-        {
+        public static void Baffle(){
             Console.Write("wug");
             Ping();
         }
-        public static void Ping()
-        {
+        public static void Ping(){
             Console.WriteLine(".");
         }
     }
