323ea67fd5f18776d1e7e4fdf8ada91bcc659011
gli <gli@SMU13041>
2024-07-08 14:42:26 -0700

    7/8 update

--- Program.cs
+++ Program.cs
@@ -1,26 +1,13 @@
-﻿// Collatz Conjecture textbook page 63 
-using System;
-public class Program
+﻿public class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter an integer (>1) to check Collatz Conjecture:");
-        int n = int.Parse(Console.ReadLine());
-        Sequence(n);
-    }
-    public static void Sequence(int n)
-    {
-        while (n != 1)
+        int i = 1;
+        while (i <= 6)
         {
-            Console.WriteLine(n);
-            if (n % 2 == 0)
-            { // n is even
-                n = n / 2;
-            }
-            else
-            { // n is odd
-                n = n * 3 + 1;
-            }
+            System.Console.Write(2 * i + " ");
+            i++;
         }
+        System.Console.WriteLine();
     }
 }
