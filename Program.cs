487c6cc9e5358f3abb0a4d36312a6aeb8b07b78f
gli <gli@SMU13041>
2024-07-08 14:47:53 -0700

    update again

--- Program.cs
+++ Program.cs
@@ -1,13 +1,6 @@
 ﻿public class Program
 {
     static void Main() {
-        //int i = 1;
-        //while (i <= 6)
-        //{
-        //    System.Console.Write(5 * i + " ");
-        //    i++;
-        //}
-        //System.Console.WriteLine();
         WriteMultiples(5);
     }
     static void WriteMultiples(int n) {
