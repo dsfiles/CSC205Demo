45c9b0f75045e72ece8d839eabee667121e2e569
gli <gli@SMU13041>
2024-07-08 14:53:43 -0700

    7/8 2:53pm

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿public class Program
 {
     static void Main() {
-        WriteMultiples(5);
+        int i = 1;
+        while (i <= 6) {
+            WriteMultiples(i);
+        }
     }
     static void WriteMultiples(int n) {
         int i = 1;
-        while (i <= 6)
-        {
+        while (i <= 6) {
             System.Console.Write(n * i + " ");
             i++;
         }
