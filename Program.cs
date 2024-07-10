f0228266147256b2614ac59e499296993e586023
gli <gli@SMU13041>
2024-07-10 14:23:03 -0700

    2:22pm

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
-﻿// The following example demonstrates the Length property.
-// https://docs.microsoft.com/en-us/dotnet/api/system.string.length?view=netcore-3.1
-using System;
-public class Program
+﻿public class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        string s = "Visual C#";
-        System.Console.WriteLine(s.Substring(7, 2));
-        // Output: "C#"
-
-        System.Console.WriteLine(s.Replace("C#", "Basic"));
-        // Output: "Visual Basic Express"
-
-        System.Console.WriteLine(s.ToUpper());
-        // Output: "VISUAL C#"
-
-        System.Console.WriteLine(s.Contains("C#"));
-        //Output: True
-
-        System.Console.WriteLine(s.Insert(0, "I like "));
-        //Output: I like Visual C#
+        string fruit = "banana";
+        int length = fruit.Length;
+        int count = 0;
+        int index = 0;
+        while (index < length)
+        {
+            if (fruit[index] == 'a')
+            {
+                count = count + 1;
+            }
+            index = index + 1;
+        }
+        System.Console.WriteLine(count);
     }
 }
