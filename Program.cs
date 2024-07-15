3272b1cb08b0a58c956b98efc2fe57389471de2f
gli <gli@SMU13041>
2024-07-15 14:56:10 -0700

    2:56pm 7/15

--- Program.cs
+++ Program.cs
@@ -1,43 +1,24 @@
 ﻿using System;
-namespace ThinkSharp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            int[] scores = RandomArray(20);
+namespace ThinkSharp{
+    public class Program {
+        public static void Main(string[] args) {
+            int[] scores = RandomArray(30);
             WriteArray(scores);
-            int a = InRange(scores, 90, 100);
-            int b = InRange(scores, 80, 90);
-            int c = InRange(scores, 70, 80);
-            int d = InRange(scores, 60, 70);
-            int f = InRange(scores, 0, 60);
-            Console.WriteLine($"{a}, {b}, {c}, {d}, {f}");
-        }
-
-        public static int InRange(int[] array, int low, int high)
-        {
-            int count = 0;
-            foreach (int item in array)
-            {
-                if (item >= low && item < high)
-                {
-                    count++;
-                }
+            int[] counts = new int[100];
+            for (int i = 0; i < scores.Length; i++) {
+                int index = scores[i];
+                counts[index]++;
             }
-            return count;
+            WriteArray(counts);
         }
-        public static int[] RandomArray(int size)
-        {
+        public static int[] RandomArray(int size) {
             Random random = new Random();
             int[] array = new int[size];
-            for (int i = 0; i < array.Length; i++)
-            {
+            for (int i = 0; i < array.Length; i++) {
                 array[i] = random.Next(0, 100);
             }
             return array;
         }
-
         public static void WriteArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
