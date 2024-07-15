7cfec39d33f234aba534006ecc35461cfe98b2b0
gli <gli@SMU13041>
2024-07-15 14:37:17 -0700

    2:37pm

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-public class Program {
-    static int a2 = 1;
-
-    public static void Main(string[] args) {
-        Random random = new Random();
-        int n1 = random.Next();
-        int n2 = random.Next(101);
-        int dice = random.Next(1, 7);
-        Console.WriteLine(n1);
-        Console.WriteLine(n2);
-        Console.WriteLine(dice);
+namespace ThinkSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteArray(RandomArray(10));
+        }
+        public static int[] RandomArray(int size)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+            return array;
+        }
+        public static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
     }
-
 }
