7fcd4fdd07814d6ef7004d7e01a007b65ebb797c
gli <gli@SMU13041>
2024-07-17 13:24:07 -0700

    7/17 1:24pm

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ namespace CSC205Demo
         public static void Main(string[] args)
         {
             Time t1 = new Time();
-            Console.WriteLine(t1);
+            Console.WriteLine(t1.ToString());
 
-            Time t2 = new Time();
+            Time t2 = new Time(1, 19);
             Console.WriteLine(t2);
 
             Time t3 = new Time(3, 50, 23);
             Console.WriteLine(t3);
+            t3.WriteTime();
+           
         }
     }
 }
--- Time.cs
+++ Time.cs
@@ -18,6 +18,13 @@ namespace CSC205Demo
             minute = 2;
             second = 3.0;
         }
+
+        public Time(int hour, int minute)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = 1.0;
+        }
         public Time(int hour, int minute, double second)
         {
             this.hour = hour;
@@ -28,6 +35,11 @@ namespace CSC205Demo
         public override string ToString()
         {
             return this.hour + ":" + this.minute + ":" + this.second;
+        }
+
+        public void WriteTime()
+        {
+            Console.WriteLine(this.hour + ":" + this.minute + ":" + this.second);
         }
 
     }
