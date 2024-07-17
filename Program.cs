8c72ae7f3d93c519688e71a6fc185634545075e0
gli <gli@SMU13041>
2024-07-17 15:28:59 -0700

    3:28pm

--- Program.cs
+++ Program.cs
@@ -3,15 +3,15 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Person person = new Person("Allan", 21);
+        //Person person = new Person("Allan", 21);
         Teacher teacher = new Teacher("Tom", 55, "Computer Science");
-        teacher.DisplayNameAge();
+        teacher.DisplayTeacher();
         Student student = new Student("Sara", 19, 3.5);
-        student.DisplayNameAge();
+        student.DisplayStudent();
     }
 }
 
-class Person // Parent class
+abstract class Person // Parent class
 {
     public string name;
     public int age;
@@ -28,10 +28,23 @@ class Teacher : Person
     public string subject;
     public Teacher(string name, int age, string subject) : base(name, age)
     { this.subject = subject; }
+
+    public void DisplayTeacher()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"Teaches {subject}\n");
+    }
+
 }
 class Student : Person
 { // Child class
     public double gpa;
     public Student(string name, int age, double gpa) : base(name, age)
     { this.gpa = gpa; }
+    public void DisplayStudent()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"GPA: {this.gpa}");
+    }
+
 }
