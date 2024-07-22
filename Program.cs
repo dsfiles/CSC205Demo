744b3cd1ba3a316ece77ac60387c622b4fae47f2
gli <gli@SMU13041>
2024-07-22 13:28:22 -0700

    7/22

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿using CSC205Demo;
-
+﻿// version 4
 public class Program
 {
     static void Main(string[] args)
     {
-        Rational r1 = new Rational();
-        Rational r2 = new Rational(1, 3);
-        Rational.WriteRational(r1);
-        Rational.WriteRational(r2);
+        Teacher teacher = new Teacher("Tom", 55, "Computer Science");
+        teacher.DisplayTeacher();
+        Student student = new Student("Sara", 19, 3.5);
+        student.DisplayStudent();
+    }
+}
+
+abstract class Person
+{ // Parent class
+    public string name;
+    public int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void DisplayNameAge()
+    { System.Console.WriteLine($"Name：{name}, Age: {age}"); }
+}
+class Teacher : Person
+{ // Child class
+    public string subject;
+    public Teacher(string name, int age, string subject) : base(name, age)
+    { this.subject = subject; }
+    public void DisplayTeacher()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"Teaches {subject}\n");
+    }
+}
+class Student : Person
+{ // Child class
+    public double gpa;
+    public Student(string name, int age, double gpa) : base(name, age)
+    { this.gpa = gpa; }
+    public void DisplayStudent()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"GPA: {this.gpa}");
     }
 }
