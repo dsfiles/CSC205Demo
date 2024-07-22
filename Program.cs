8f09ee1fa3cb9802e3b291ad55dace8955c1214b
gli <gli@SMU13041>
2024-07-22 14:47:45 -0700

    2:50pm

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-public static class StaticClass
-{
+public static class StaticClass {
     public static string staticStr = "A static variable (or data member)";
     public static void StaticMethod()
     {
         Console.WriteLine(staticStr);
     }
 }
-public class Program
-{
+public class Program {
     static void Main(string[] args)
     {
         // StaticClass sc = new StaticClass(); // can’t be instantiated!
