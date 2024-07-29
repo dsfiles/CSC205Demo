3f9a7914e396baa8ef85aa8c2bdb6ba4f14de4b0
gli <gli@SMU13041>
2024-07-29 15:08:42 -0700

    C# struct

--- Program.cs
+++ Program.cs
@@ -1,39 +1,18 @@
 ﻿using System;
-
-namespace CSC205Quiz02
+public struct Book // change struct to class to see the difference
 {
-    public class Program
+    public string title, isbn;
+    public double price;
+}
+public class Program
+{
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // car1 does not have any information.
-            var car1 = new Vehicle();
-            var car2 = new Vehicle() { Vin = "1FM0122021", Make = "Ford", Model = "Mercury", Mileage = 0 };
-            car2.Drive(100);
-            Console.Write("Car #1: ");
-            car1.Display();
-            Console.Write("Car #2: ");
-            car2.Display();
-            car2.Drive(90);
-            Console.Write("After driving car #2 90 miles\nCar #2: ");
-            car2.Display();
-        }
-    }
-
-    public class Vehicle
-    {
-        public string Vin { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Mileage { get; set; }
-
-        public void Drive(int miles)
-        {
-            this.Mileage += miles;
-        }
-        public void Display()
-        {
-            Console.WriteLine($"Vin: {Vin}, make: {Make}, Model: {Model}, Mileage: {Mileage}");
-        }
+        Book book1 = new Book();
+        Console.WriteLine(book1.price);
+        book1.title = "C# Programming";
+        book1.isbn = "180056810X";
+        book1.price = 42.74;
+        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
     }
 }
