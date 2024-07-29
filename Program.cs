5e10f97455656bab26ec5387a886076261b1a58d
gli <gli@SMU13041>
2024-07-29 15:34:12 -0700

    3:36pm

--- Program.cs
+++ Program.cs
@@ -5,19 +5,16 @@ abstract class Shape
     public abstract int GetArea();
 }
 
-class Square : Shape
-{
+class Square : Shape {
     private int _side;
-
     public Square(int n) => _side = n;
-
-    // GetArea method is required to avoid a compile-time error.
     public override int GetArea() => _side * _side;
+ 
+}
 
-    static void Main()
-    {
+class App {
+    static void Main() {
         var sq = new Square(5);
         Console.WriteLine($"Area of the square = {sq.GetArea()}");
     }
 }
-// Output: Area of the square = 144z
