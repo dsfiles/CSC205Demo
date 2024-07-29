b86aa9c65c00c5fc22fe9659a6577cbdbea1f2b6
gli <gli@SMU13041>
2024-07-29 15:36:56 -0700

    3:35pm

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-
-abstract class Shape
-{
+abstract class Shape {
     public abstract int GetArea();
 }
-
 class Square : Shape {
     private int _side;
     public Square(int n) => _side = n;
     public override int GetArea() => _side * _side;
- 
 }
-
+class Rectangle : Shape {
+    // your code here
+}
 class App {
     static void Main() {
         var sq = new Square(5);
         Console.WriteLine($"Area of the square = {sq.GetArea()}");
+        //var rec = new Rectangle(3, 4);
+        //Console.WriteLine($"Area of the rectangle = {rec.GetArea()}");
     }
 }
