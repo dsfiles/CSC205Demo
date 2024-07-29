9d579a32400f918364ecaa5a877b2dddb8347a37
gli <gli@SMU13041>
2024-07-29 13:52:40 -0700

    Car class

--- Program.cs
+++ Program.cs
@@ -1,32 +1,49 @@
 ﻿using System;
-class Car
+
+namespace CSC205Quiz02
 {
-    private string vin, make, model;
-    private int mileage;
-    public Car(string vin, string make, string model, int mileage=0)
+    public class Program
     {
-        this.vin = vin;
-        this.make = make;
-        this.model = model;
-        this.mileage = mileage;
+        static void Main(string[] args)
+        {
+            // car1 does not have any information.
+            var car1 = new Vehicle();
+            var car2 = new Vehicle("1FM0122021", "Ford", "Mercury", 0);
+            car2.Drive(100);
+            Console.Write("Car #1: ");
+            car1.Display();
+            Console.Write("Car #2: ");
+            car2.Display();
+            car2.Drive(90);
+            Console.Write("After driving car #2 90 miles\nCar #2: ");
+            car2.Display();
+        }
     }
 
-    public void Drive(int miles) => mileage += (miles > 0) ? miles : 0;
-
-    public void Display() => Console.WriteLine($"VIN: {vin}\nMake: {make}\nModel: {model}\nMileage: {mileage}\n");
-}
-
-class Program
-{
-   static void Main()
+    public class Vehicle
     {
-        var car1 = new Car("2HGCM82633A004353", "Kia", "Sorento", 30000);
-        var car2 = new Car("3FTFW1EF1EKE89766", "Nissan", "Titan XD");
-        car1.Drive(120);
-        car2.Drive(180);
-
-        car1.Display();
-        car2.Display();
+        private string vin, make, model;
+        private int mileage;
+        public Vehicle() // constructor #1
+        {
+            vin = "N/A"; make = "N/A"; model = "N/A";
+            mileage = 0;
+        }
+        public Vehicle(string vin, string make, string model, int mileage)
+        {  // constructor #2 with parameters
+            this.vin = vin;
+            this.make = make;
+            this.model = model;
+            this.mileage = mileage;
+        }
+        public void Drive(int mileage)
+        {
+            this.mileage += mileage;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"vin: {vin}, make: {make}, model: {model}, mileage: {mileage}");
+        }
     }
 }
 
