3f77773e03ff797ca7ec1927b316df6d461be952
gli <gli@SMU13041>
2024-07-31 14:25:05 -0700

    2:25pm

--- Program.cs
+++ Program.cs
@@ -8,34 +8,18 @@ class TestClass
         public abstract double Area();
     }
     public class Circle : Shape {
-        //double r;
-        //public Circle(double r) {
-        //    this.r = r;
-        //}
-        //public override double Area()
-        //{
-        //    return PI * r * r;
-        //}
         public double R {  get; set; }
         public override double Area() => PI * R * R;
-
     }
     public class Cylinder : Shape {
-        private double r, h;
-        public Cylinder(double r, double h){
-            this.r = r;
-            this.h = h;
-        }
-        public override double Area()
-        {
-            return 2 * PI * r * r + 2 * PI * r * h;
-        }
+        public double R {  get; set; }
+        public double H { get; set; }
+        public override double Area() => 2 * PI * R * R + 2 * PI * R * H;
     }
-    static void Main()
-    {
+    static void Main() {
         double r = 3.0, h = 5.0;
-        Shape c = new Circle() {R=r};
-        Shape l = new Cylinder(r, h);
+        Shape c = new Circle() {R = r};
+        Shape l = new Cylinder() { R = r, H = h };
         // Display results.
         Console.WriteLine("Area of Circle   = {0:F1}", c.Area());
         Console.WriteLine("Area of Cylinder = {0:F2}", l.Area());
