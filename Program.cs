464fee1a5fa58e8448d46a75822b3e4ab1075856
gli <gli@SMU13041>
2024-07-31 13:31:09 -0700

    7/31

--- Program.cs
+++ Program.cs
@@ -1,27 +1,45 @@
 ﻿using System;
-abstract class Shape {
-    public abstract int GetArea();
-}
-class Square : Shape {
-    private int _side;
-    public Square(int n) => _side = n;
-    public override int GetArea() => _side * _side;
-}
-class Rectangle : Shape {
-    private int _length, _width;
-    public Rectangle(int length, int width)
-    { 
-        _length = length;
-        _width = width;
+
+class TestClass
+{
+    public class Shape
+    {
+        public const double PI = Math.PI;
+        protected double _x, _y;
+        public Shape(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public virtual double Area()
+        {
+            return _x * _y;
+        }
     }
-    public override int GetArea() => _length * _width;
-}
-class App {
-    static void Main() {
-        Shape[] shapes = { new Square(5), new Rectangle(3, 4) };
-        //Shape sq = new Square(5);
-        Console.WriteLine($"Area of the square = {shapes[0].GetArea()}");
-        //Shape rec = new Rectangle(3, 4);
-        Console.WriteLine($"Area of the rectangle = {shapes[1].GetArea()}");
+    public class Circle : Shape
+    {
+        public Circle(double r) : base(r, 0) { }
+
+        public override double Area()
+        {
+            return PI * _x * _x;
+        }
+    }
+    public class Cylinder : Shape
+    {
+        public Cylinder(double r, double h) : base(r, h) { }
+        public override double Area()
+        {
+            return 2 * PI * _x * _x + 2 * PI * _x * _y;
+        }
+    }
+    static void Main()
+    {
+        double r = 3.0, h = 5.0;
+        Shape c = new Circle(r);
+        Shape l = new Cylinder(r, h);
+        // Display results.
+        Console.WriteLine("Area of Circle   = {0:F2}", c.Area());
+        Console.WriteLine("Area of Cylinder = {0:F2}", l.Area());
     }
 }
