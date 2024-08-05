2e7eca69c06f1e644035784d613623f6d9d2b466
gli <gli@SMU13041>
2024-08-05 15:32:09 -0700

    stack/queue

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-class Example
-{
-    public static void Main()
-    {
-        Queue<string> numbers = new Queue<string>();
+class Example {
+    public static void Main() {
+        var numbers = new Queue<string>();
         numbers.Enqueue("one");
         numbers.Enqueue("two");
         numbers.Enqueue("three");
         numbers.Enqueue("four");
         // A queue can be enumerated without disturbing its contents.
-        foreach (string number in numbers)
+        Console.WriteLine("Contents of queue:");
+        foreach (var number in numbers) {
+            Console.WriteLine(number);
+        }
+        
+        var numbersStack = new Stack<string>();
+        foreach (var number in numbers) {
+            numbersStack.Push(number);
+        }
+       
+        Console.WriteLine("Contents of stack:");
+        foreach (var number in numbersStack) {
+            Console.WriteLine(number); ;
+        }
+
+        Console.WriteLine("Contents of queue again:");
+        foreach (var number in numbers)
         {
             Console.WriteLine(number);
         }
-        Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
-        Console.WriteLine("Peek at next item to dequeue: {0}", numbers.Peek());
-        Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
-        Console.WriteLine("\nItems in the queue now:");
-        foreach (string number in numbers)
-        {
-            Console.WriteLine(number);
-        }
+
     }
 }
-// https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1
