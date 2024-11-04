2ed74dbd287525c9a98162a9f40df91c3235fcac
gli <gli@SMU13041>
2024-11-04 11:00:00 -0800

    update 11/4

--- Program.cs
+++ Program.cs
@@ -1,40 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-public class Example
+
+public class Vehicle // base class
 {
-    public static void Main()
+    public string model;
+    public int year;
+    public Vehicle(string model, int year)
     {
-        // Create the link list.
-        string[] words =
-            { "the", "fox", "jumps", "over", "the", "dog" };
-        LinkedList<string> sentence = new LinkedList<string>(words);
-        Display(sentence, "The linked list values:");
-        Console.WriteLine("sentence.Contains(\"jumps\") = {0}", sentence.Contains("jumps"));
-        // Add the word 'today' to the beginning of the linked list.
-        sentence.AddFirst("today");
-        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
-        // Move the first node to be the last node.
-        LinkedListNode<string> mark1 = sentence.First;
-        sentence.RemoveFirst();
-        sentence.AddLast(mark1);
-        Display(sentence, "Test 2: Move first node to be last node:");
-        // Change the last node to 'yesterday'.
-        sentence.RemoveLast();
-        sentence.AddLast("yesterday");
-        Display(sentence, "Test 3: Change the last node to 'yesterday':");
-        // Move the last node to be the first node.
-        mark1 = sentence.Last;
-        sentence.RemoveLast();
-        sentence.AddFirst(mark1);
-        Display(sentence, "Test 4: Move last node to be first node:");
+        this.model = model;
+        this.year = year;
     }
-    private static void Display(LinkedList<string> words, string test)
+    public void DisplayModelYear()
     {
-        Console.WriteLine(test);
-        foreach (string word in words)
-        {
-            Console.Write(word + " ");
-        }
-        Console.Write("\n\n");
+        Console.WriteLine($"model: {model}, year: {year}");
     }
-} // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-8.0
+}
+
+public class Car : Vehicle
+{
+    private int seating_capacity;
+    public Car(string model, int year, int capacity) : base(model, year)
+    {
+        seating_capacity = capacity;
+    }
+    public void DisplayInfo()
+    {
+        DisplayModelYear();
+        Console.WriteLine($"seating capacity: {seating_capacity}");
+    }
+}
+public class Truck : Vehicle {
+    private int load_capacity;
+    public Truck(string model, int year, int capacity) : base(model, year)
+    {
+        load_capacity = capacity;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"model: {model}, year: {year}, load capacity: {load_capacity} tons");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Honda Accord", 2019, 5);
+        var truck = new Truck("kenworth W990", 2020, 50);
+        car.DisplayInfo();
+        //truck.DisplayInfo();
+    }
+}
+
