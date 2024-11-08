c1bdf20b393d9875fc09aac6733348e23cdc5766
gli <gli@SMU13041>
2024-11-08 09:19:40 -0800

    update 11/8

--- Program.cs
+++ Program.cs
@@ -1,62 +1,16 @@
-﻿// version 7 – Properties
+﻿// Abstract class
 using System;
+class Sort { 
+    static void Main()
+    {
+        int[] a = { 2, 3, 7, 9 };
+        int[] b = { 0, 1, 4, 8 };
+        int[] c = new int[8];
+        MergeArray(a, b, c);
+    }
 
-public class Program
-{
-    static void Main(string[] args)
+    static void MergeArray(int[] a, int[] b, int[] c)
     {
-        Teacher teacher = new Teacher("Tom", 55, "Computer Science");
-        teacher.DisplayInfo();
-        Student student = new Student("Sara", 19, 3.5);
-        student.DisplayInfo();
-        student.Gpa = 5.8;
-        student.DisplayInfo();
-        Console.WriteLine(student.Gpa);
-    }
-}
 
-abstract class Person
-{ // Parent class
-    private string name;
-    private int age;
-    public Person(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
-    }
-    public void DisplayNameAge()
-    { System.Console.WriteLine($"Name：{name}, Age: {age}"); }
-}
-class Teacher : Person
-{ // Child class
-    private string subject;
-    public Teacher(string name, int age, string subject) : base(name, age)
-    { this.subject = subject; }
-    public void DisplayInfo()
-    {
-        base.DisplayNameAge();
-        System.Console.WriteLine($"Teaches {subject}\n");
-    }
-}
-class Student : Person
-{ // Child class
-    private double gpa;
-    public double Gpa { 
-        get 
-        { 
-            return gpa; 
-        } 
-        set 
-        {
-            if (value >= 0.0 && value <= 4.0)
-            { gpa = value; }
-        } 
-    } // Just a Property example
-    public Student(string name, int age, double gpa) : base(name, age)
-    { this.gpa = 3.5; }
-    public void DisplayInfo()
-    {
-        base.DisplayNameAge();
-        System.Console.WriteLine($"GPA: {this.gpa}");
     }
 }
