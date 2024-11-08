1b882e84414f2fc7d79b8950337ac7149c8dddbb
gli <gli@SMU13041>
2024-11-08 10:53:46 -0800

    merge sort works

--- Program.cs
+++ Program.cs
@@ -4,11 +4,8 @@ class Sort {
     static void Main()
     {
         int[] x = { 5, 1, 2, 4, 0, -10 };
-        int[] a = { 2, 3, 7, 9 };
-        int[] b = { -60, 1, 4, 8, 10, 20, 30 };
-        int[] c = new int[a.Length + b.Length];
-        Merge(a, b, c);
-        foreach (int item in c)
+        MergeSort(x);
+        foreach (int item in x)
         {
             Console.WriteLine(item);
         }
@@ -16,15 +13,20 @@ class Sort {
 
     static void MergeSort(int[] x)
     {
-        if (x.Length == 1) return;
+        if (x.Length == 1 || x.Length ==0) return;
+        
         int mid = x.Length / 2;
         
         int[] p = new int[mid];
         for (int i = 0; i < mid; i++) p[i] = x[i];
-
+        ;
         int[] q = new int[x.Length-mid];
-        for (int i = 0; q[i] < x.Length;i++) q[i] = x[i];    
-
+        for (int i = mid; i < x.Length;i++) q[i-mid] = x[i];
+        ;
+        MergeSort(p);
+        MergeSort(q);
+        ;
+        Merge(p, q, x); 
 
     }
     static void Merge(int[] a, int[] b, int[] c)
