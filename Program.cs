28e9e1affe2bbc773e72fa10af9e1b3dc5babfa4
gli <gli@SMU13041>
2024-11-08 11:44:54 -0800

    final update: merge sort does not work

--- Program.cs
+++ Program.cs
@@ -3,46 +3,42 @@ using System;
 class Sort { 
     static void Main()
     {
-        int[] x = { 5, 1, 2, 4, 0, -10 };
-        MergeSort(x);
+        int[] x = { 5, 1, 2, 4, 0};
+        MergeSort(0, x.Length-1, x);
         foreach (int item in x)
         {
             Console.WriteLine(item);
         }
     }
-
-    static void MergeSort(int[] x)
+    static void MergeSort(int left, int right, int[] x)
     {
-        if (x.Length == 1 || x.Length ==0) return;
-        
-        int mid = x.Length / 2;
-        
-        int[] p = new int[mid];
-        for (int i = 0; i < mid; i++) p[i] = x[i];
-        ;
-        int[] q = new int[x.Length-mid];
-        for (int i = mid; i < x.Length;i++) q[i-mid] = x[i];
-        ;
-        MergeSort(p);
-        MergeSort(q);
-        ;
-        Merge(p, q, x); 
+        if (left >= right) return;
 
+        int mid = (left + right) / 2;
+        
+        MergeSort(left, mid, x);
+        MergeSort(mid+1, right, x);
+        
+        Merge(left, right, mid, x); 
     }
-    static void Merge(int[] a, int[] b, int[] c)
-    {
-        int i = 0, j = 0, k = 0;
-        while (i<a.Length & j<b.Length)
-        {
-            if(a[i] < b[j])
-                c[k++] = a[i++];            
+    static void Merge(int left, int right, int mid, int[] c) {
+        int[] m = new int[c.Length];
+       
+        int i = 0, j = mid, k = 0;
+        
+        while (i<=mid-1 & j<c.Length) {
+            if(c[i] < c[j])
+                m[k++] = c[i++];            
             else
-                c[k++] = b[j++];
+                m[k++] = c[j++];
         }
-        while (i < a.Length)
-            c[k++] = a[i++];   
+        while (i < c.Length)
+            m[k++] = c[i++];   
 
-        while (j < b.Length)
-            c[k++] = b[j++];
+        while (j < c.Length)
+            m[k++] = c[j++];
+
+        c = m;
+
     }
 }
