eed644f1bff2dc1dc824c921ab81f98b0c479971
gli <gli@SMU13041>
2024-11-08 09:47:46 -0800

    update again

--- Program.cs
+++ Program.cs
@@ -7,10 +7,28 @@ class Sort {
         int[] b = { 0, 1, 4, 8 };
         int[] c = new int[8];
         MergeArray(a, b, c);
+        foreach (int item in c)
+        {
+            Console.WriteLine(item);
+        }
     }
 
     static void MergeArray(int[] a, int[] b, int[] c)
     {
+        int i = 0, j = 0, k = 0;
+        while (i<4 & j<4)
+        {
+            if(a[i] < b[j])
+            {
+                c[k] = a[i];
+                i++; k++;
+            }
+            else
+            {
+                c[k] = b[j];
+                j++; k++;
+            }
+        }
 
     }
 }
