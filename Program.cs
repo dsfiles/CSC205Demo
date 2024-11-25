64924d4ee3182327ac803e28968a767a81fcd7bc
gli <gli@SMU13041>
2024-11-25 10:22:39 -0800

    11/25

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-public struct Book {
-    public string title, isbn;
-    public double price;
-    public static void IncreasePrice(Book b)
+
+public class Program
+{
+    public static void Main()
     {
-        b.price += 10.0;
-        Console.WriteLine("price increased by $10!");
+        int[] arr = { 1, 3, 5, 7, 9 };
     }
-}
-public class Program {
-    public static void Main(string[] args) {
-        //Book book1;
-        Book book1 = new Book();
-        //Console.WriteLine(book1.price);
-        book1.title = "C# Programming";
-        book1.isbn = "180056810X";
-        book1.price = 42.74;
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
-        Book.IncreasePrice(book1);
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
+
+    static void Insert(int[], int x) {
+        // your code here
     }
 }
