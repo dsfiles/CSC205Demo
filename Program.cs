8fc20c980fd6616ba656cc53eb28deed04e90288
gli <gli@SMU13041>
2024-11-25 10:32:54 -0800

    update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@ public class Program
     public static void Main()
     {
         int[] arr = { 1, 3, 5, 7, 9 };
+        Print(arr, "Original array:"); 
+
+    }
+
+    static void Print(int[] arr, string msg)
+    {
+        Console.WriteLine(msg);
+        foreach (int i in arr) Console.Write(i + " ");
+        Console.WriteLine();
     }
 
     static void Insert(int[] arr, int x, int index) {
@@ -15,5 +24,6 @@ public class Program
     {
         // your code here to remove the element at "index"
     }
+
 
 }
