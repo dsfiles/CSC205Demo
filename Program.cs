17c99556893969b862e0449aef057268170da2f1
gli <gli@SMU13041>
2024-11-26 09:39:45 -0800

    update

--- Program.cs
+++ Program.cs
@@ -18,5 +18,42 @@ public class Program {
         node1.Value = 2;
         node2.Value = 3;
         node3.Value = 4;
+
+        //node1.Prev = head;
+        //node3.Next = tail;
+
+        node1.Next = node2;
+        node2.Prev = node1;
+        node2.Next = node3;
+        node3.Prev = node2;
+
+        //Console.WriteLine(head.Value);
+        //Console.WriteLine(head.Next.Value);
+        //Console.WriteLine(head.Next.Next.Value);
+
+
+
+
+        
+
+        Node iterator = head;
+        while (iterator != null)
+        {
+            Console.Write(iterator.Value + " ");
+            iterator = iterator.Next;
+        }
+        Console.WriteLine();
+
+        iterator = tail;
+        while (iterator != null)
+        {
+            Console.Write(iterator.Value + " ");
+            iterator = iterator.Prev;
+        }
+        Console.WriteLine();
+
+        Console.WriteLine(head.Next.Next.Prev.Next.Value);
+        Console.WriteLine(tail.Prev.Prev.Next.Value);
+
     }
 }
